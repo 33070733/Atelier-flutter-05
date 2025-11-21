f47b9ad60f2b9e5ed51bf4a0e740220be5210730
33070733 <rabiabouhali01@gmail.com>
2025-11-21 17:36:34 -0500

    v3.6

--- lib/page_infos.dart
+++ lib/page_infos.dart
@@ -15,6 +15,13 @@ class PageInfos extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Text("Bienvenue sur la page la page Infos !"),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text("Page précédente"),
+              ),
             ],
           ),
         ),
